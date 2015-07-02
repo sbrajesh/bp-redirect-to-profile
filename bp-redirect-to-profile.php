5fb8458bd6feebf3e9b3289be46dfecd818ede13
Brajesh Singh <brajesh@cosmiccoders.com>
2015-07-02 19:45:54 +0530

    Cleanup

--- bp-redirect-to-profile.php
+++ bp-redirect-to-profile.php
@@ -1,39 +1,48 @@
 <?php
+
 /*
-Plugin Name: BP Redirect to Profile for Buddypress
-Description:Redirect user to their profile when they login
-Version: 1.1.1
-Requires at least: BuddyPress 1.1
-Tested up to: BuddyPress 1.5+WordPress 3.2.1
-License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
-Author: Brajesh Singh
-Author URI: http://buddydev.com
-Plugin URI:http://buddydev.com/buddypress/bp-redirect-to-profile-plugin-redirect-users-to-their-profile-on-login-on-buddypress-sites/
-Last updated:16th feb 2010
-*/
-/***
-    Copyright (C) 2009-2011 Brajesh Singh(buddydev.com)
+  Plugin Name: BP Redirect to Profile for Buddypress
+  Description:Redirect user to their profile when they login
+  Version: 1.2
+  Requires at least: BuddyPress 1.1
+  Tested up to: BuddyPress 2.3.2.1
+  License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
+  Author: Brajesh Singh
+  Author URI: http://buddydev.com
+  Plugin URI:http://buddydev.com/buddypress/bp-redirect-to-profile-plugin-redirect-users-to-their-profile-on-login-on-buddypress-sites/
+  Last updated: July 2, 2015
+ */
+/* * *
+  Copyright (C) 2009-2011 Brajesh Singh(buddydev.com)
 
-    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or  any later version.
+  This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or  any later version.
 
-    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
+  You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses>.
 
-    */
-/*Add a filter to filter the redirect url for login*/
-add_filter("login_redirect","bpdev_redirect_to_profile",100,3);
+ */
 
-function bpdev_redirect_to_profile($redirect_to_calculated,$redirect_url_specified,$user){
-/*if no redirect was specified,let us think ,user wants to be in wp-dashboard*/
-if(empty($redirect_to_calculated))
-	$redirect_to_calculated=admin_url();
+/* Add a filter to filter the redirect url for login */
+add_filter( 'login_redirect', 'bpdev_redirect_to_profile', 100, 3 );
+
+function bpdev_redirect_to_profile ( $redirect_to_calculated, $redirect_url_specified, $user ) {
 	
-	/*if the user is not super admin,redirect to his/her profile*/
-	if(!is_super_admin($user->user_login))
-		return apply_filters("bpdev_login_redicrect_url",bp_core_get_user_domain($user->ID ),$user->ID);//allow top redirect at other place if they want
-	else
-		return $redirect_to_calculated; /*if site admin or not logged in,do not do anything much*/
-
+	/* if no redirect was specified, let us assume that the user wants to be in wp-dashboard */
+	if ( empty( $redirect_to_calculated ) ) {
+	
+		$redirect_to_calculated = admin_url();
+		
+	}
+	
+	// if the user is not site admin, redirect to his/her profile 
+	if ( ! is_super_admin( $user->ID ) ) {
+		
+		return bp_core_get_user_domain( $user->ID );
+		
+	} else {
+		//if site admin or not logged in, do not do anything much 
+		return $redirect_to_calculated; 
+		
+	}	
 }
-?>
--- readme.txt
+++ readme.txt
@@ -1,20 +1,21 @@
 === BP Redirect to Profile ===
-Contributors:sbrajesh
+Contributors: buddydev, sbrajesh
 Donate link: http://buddydev.com/donate/
-Tags: buddypress,buddypress login,redirect,redirect to profile
-Requires at least: wpmu 2.8+buddypress 1.1
-Tested up to:wpmu 2.9.1 +buddypress 1.2
-Stable tag: 1.1
+Tags: buddypress, buddypress login, redirect,redirect to profile, buddypress login redirect
+Requires at least: BuddyPress 1.1
+Tested up to:BuddyPress 2.3.2.1
+Stable tag: 1.2
 
-Bp redirect to profile is a buddypress plugin which redirects a user to their profile on login.
+BuddyPress Redirect to Profile is a buddypress plugin which redirects a user to their profile on login.
 == Description ==
-Bp Redirect to Profile is a plugin for buddypress +wpmu based site, which enables site admins to redirect users to their profile on login. It's simple and helps to provide a better user experience.
+
+BuddyPress Login redirect plugin redirects user to their profile on a login, on a BuddyPress based social site. It's simple and helps to provide a better user experience.
 
 Features include:
 
 * Redirect all normal users to their profile on login
-* If the admin logs in from site front page,redirect him back to front page.
-* If the Site Admin logs in from site backend, i.e. using http://yoursite.com/wp-login.php, let him go to the wordpress dashboard
+* If the admin logs in from site front page, redirect him back to front page.
+* If the Site Admin logs in from site backend, i.e. using http://yoursite.com/wp-login.php, let him go to the WordPress dashboard
 
 == Installation ==
 
@@ -26,15 +27,24 @@ The plugin is simple to install:
 1. Go to the plugin management page and enable the plugin, Activate the plugin sitewide
 
 Otherwise, Use the Plugin browser, upload it and activate, you are done.
-Please note, The plugin must be activated sitewide.
+Please note, The plugin must be activated network wide if you are using multisite.
+
 == Frequently Asked Questions ==
 = Does It stops Users from getting inside the wordpress dashboard =
 Yes, and No both.It does not allow user to directly go to the wordpress Mu backend, but if a user manually tries to go to the wordpress backend, It does not stops it.
 This plugin's sole purpose is to redirect users to their buddypress profile on login.
+
 == Changelog == 
-Version 1.0 released
-Version 1.1
+=== 1.2 ===
+Code cleanup and testing with BuddyPress 2.3+
+
+=== 1.1 === 
 Support for Buddypress 1.2 added
+
+=== 1.0 ===
+Initial release
+
+
 == Other ==
 
-For more info visit us at [BuddyDev.com](http://buddydev.com/ "The best place for all buddypress based plugins,themes tutorials")
+For more info visit us at [BuddyDev.com](http://buddydev.com/ "The best place for all BuddyPress based plugins, themes tutorials")
